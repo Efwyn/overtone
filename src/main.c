473ee65d8058d4e2d6a38aa21b22781190f7b0bb
Morgan Carpenetti <efwyn.dev@gmail.com>
2026-04-19 04:05:03 -0700

    added cmake, hello overtone!

---
+++ src/main.c
@@ -0,0 +1,14 @@
+// ========================================
+// File: main.c
+// Description: Overtone: A Game Engine Written in C
+// Author: Morgan Carpenetti
+// Created On: 19-04-2026
+// ========================================
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+    printf("Hello Overtone!\n");
+    return 0;
+}
+
